d6a8342879a0fd761b6bcd12b9efbddc349885a0
Krmjn09 <mahajankriti672@gmail.com>
2025-07-31 22:01:55 +0530

    frontend

---
+++ frontend/src/app/components/blog-create/blog-create.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-create',
+  imports: [],
+  templateUrl: './blog-create.html',
+  styleUrl: './blog-create.scss'
+})
+export class BlogCreate {
+
+}
